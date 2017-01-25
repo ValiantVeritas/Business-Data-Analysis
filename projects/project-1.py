90c4eeb3166dc7da7e275a8614dcf4874f8c3f59
ValiantVeritas <valiantveritas@gmail.com>
2017-01-24 21:19:08 -0800

    Updated README.md and changed name of demo copy.py to project-1.py

--- projects/project-1.py
+++ projects/project-1.py
@@ -1,3 +1,8 @@
+# This project trains a model with an array containing body measurements 
+# and classifying each element as either male or female.
+# the resulting model is then used to make predictions on whether a given array of body measurements
+# belongs to a male or female.
+
 from sklearn import tree
 
 x = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 63, 40], [190, 90, 47], [177, 70,40], [159, 55, 37], [171, 75, 42], [180, 80, 42]] 
